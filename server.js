c87f9ab14d8e4d9a18e26061af59d0e95f4d774b
John Clark <socrates37@gmail.com>
2017-02-18 07:20:27 -0700

    roughed in local POST interception

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 self.importScripts('store.js');
 
+self.onmessage = function(e) {
+  const request = JSON.parse(e.data);
+  console.log('request', request.id);
+
+  postMessage(JSON.stringify({
+    id: request.id,
+    data: {
+      status: 200,
+      body: JSON.stringify({working: true}),
+    }
+  }));
+}
+
 console.log('server started');
